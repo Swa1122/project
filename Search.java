e6d528a81450ac60e251c066a001c8c73602fa33
Swa <98945043+Swa1122@users.noreply.github.com>
2022-02-22 16:05:26 +0530

    Add files via upload

---
+++ Search.java
@@ -0,0 +1,69 @@
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+  public class Search {
+
+	public static void main(String args[]){
+	
+    Connection con  = null;
+	Statement stmt = null;
+	ResultSet  rs  = null;
+	
+	try{
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		String url="jdbc:oracle:thin:@localhost:1521:XE";
+		con  = DriverManager.getConnection(url,"system","root");
+		stmt  = con.createStatement();
+		
+		String sql="select * from student where student_no=102 "  ;//
+		rs  = stmt.executeQuery(sql);
+			System.out.println("________________________________________");
+		while(rs.next()){
+		int id = rs.getInt("student_no");		
+		String name = rs.getString("student_name");		
+		String dob =rs.getString("dob");
+		String doj =rs.getString("doj");
+			
+		System.out.printf("%-5d %-10s %-10s %-10s\n",id,name,dob,doj);
+			
+		System.out.println("________________________________________");
+					
+			
+		}
+		
+		
+		
+		
+		
+		
+	   
+    }catch(ClassNotFoundException e){
+		System.out.println("DriverERROR ::Driver class not loaded "+e.getMessage() );
+	}catch(SQLException e){
+		System.out.println("DB ERROR  "+e.getMessage() );
+	}catch(Exception e){
+		System.out.println( " Other ERROR   "+e.getMessage() );
+	}finally{
+	    if(con!=null ){
+		  try{
+		  con.close();
+		    System.out.println("con close successfuly");
+		
+		}catch(SQLException e){
+		    System.out.println("DB Close ERROR " + e.getMessage());
+		  }
+		}
+	}//finally end
+	
+	
+	
+	
+	  
+	}//end main
+
+}//end class
+ 
